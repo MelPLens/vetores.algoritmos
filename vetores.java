25a11869807de7193de8093632ef0bcd30a56c8e
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2022-06-13 20:10:40 -0300

    Create vetores.java

---
+++ vetores.java
@@ -0,0 +1,39 @@
+package listajunho;
+
+import java.io.InputStream;
+
+public class Scanner {
+
+	}
+
+public abstract class vetoresa{
+public  void main (String[] args) {
+}
+
+Scanner ler = new Scanner (); 
+int vetor1 [] = {0,0,0,0,0};
+int vetor2 [] = {0,0,0,0,0};
+int x = 0 ;
+int c = 0 ;
+
+}	
+do {
+
+System.out.print(5:"digite o valor de A");
+a[x]= ler.nextInt();
+System.out.print(5:"digite o valor de A");
+b[x]= ler.nextInt();
+
+x++;
+
+}
+while (x<5);
+x=0;
+c=0;
+
+do {
+c= c+(a[x])  * b[x]);
+x++;
+} while (x<5);
+
+System.out.print(5:"digite o valor de A");
